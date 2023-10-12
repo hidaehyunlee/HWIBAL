0af34bd3f9fa1a2c7f13caf88d77086bc3a0a22f
Seoon KIM <6872065@gmail.com>
2023-10-12 19:42:21 +0900

    [FEAT] : 👍 MyPage 네비게이션 라지타이틀로 변경

--- HWIBAL/Controllers/HomeViewController.swift
+++ HWIBAL/Controllers/HomeViewController.swift
@@ -15,9 +15,19 @@ struct PushToMyPageScreenEvent: EventProtocol {
 final class HomeViewController: RootViewController<HomeView> {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         EventBus.shared.on(PushToMyPageScreenEvent.self, by: self) { listener, _ in
             listener.navigationController?.pushViewController(MyPageViewController(), animated: true)
         }
+    }
+}
+
+private extension HomeViewController {
+    func setup() {
+        
+        // MARK: - Navigation Setting
+        let backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+            backBarButtonItem.tintColor = .black
+            self.navigationItem.backBarButtonItem = backBarButtonItem
     }
 }
--- HWIBAL/Controllers/MyPageViewController.swift
+++ HWIBAL/Controllers/MyPageViewController.swift
@@ -10,5 +10,16 @@ import UIKit
 final class MyPageViewController: RootViewController<MyPageView> {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup()
+    }
+}
+
+private extension MyPageViewController {
+    func setup() {
+        
+        // MARK: - Navigation Setting
+        navigationItem.title = "내 정보"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
