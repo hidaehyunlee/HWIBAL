0028d0b062c4ea8b39fd3cdaeed5d36fefe5bbe5
hidaehyunlee <hidaehyunlee@gmail.com>
2023-10-13 19:59:16 +0900

    [FEAT]: 👍: SignInView Component 개발

--- HWIBAL/Views/SignInView/CustomGIDSignInButton.swift
+++ HWIBAL/Views/SignInView/CustomGIDSignInButton.swift
@@ -5,4 +5,27 @@
 //  Created by daelee on 10/13/23.
 //
 
-import Foundation
+import GoogleSignIn
+
+class CustomGIDSignInButton: GIDSignInButton {
+    override init() {
+        super.init() // Objective-C의 초기화 규칙을 적용하기 위해 super.init()을 호출합니다.
+        configureButton()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureButton()
+    }
+
+    private func configureButton() {
+        // 버튼 문구 변경
+        self.setTitle("내 버튼 문구", for: .normal)
+        // 그림자 효과 제거
+        self.layer.shadowOpacity = 0
+        // 테두리 라운드 적용
+        self.layer.cornerRadius = 4
+    }
+}
+
+
--- HWIBAL/Views/SignInView/SignInView.swift
+++ HWIBAL/Views/SignInView/SignInView.swift
@@ -12,11 +12,29 @@ import UIKit
 final class SignInView: UIView, RootView {
     var onGoogleSignInTapped: (() -> Void)?
 
-    private let googleSignInButton: GIDSignInButton = {
+    private lazy var hwibariImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "hwibari_01"))
+
+        return imageView
+    }()
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+
+        label.text = "아, 휘발\n나만의 안전한 공간"
+        label.textColor = UIColor(red: 0.451, green: 0.306, blue: 0.969, alpha: 1) // 추후 변경
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.numberOfLines = 2
+
+        return label
+    }()
+    
+    private lazy var googleSignInButton: GIDSignInButton = {
         let button = GIDSignInButton()
 
         button.colorScheme = .light
         button.style = .wide
+        button.addTarget(self, action: #selector(handleGoogleSignIn), for: .touchUpInside)
 
         return button
     }()
@@ -25,10 +43,30 @@ final class SignInView: UIView, RootView {
         backgroundColor = .systemBackground
 
         addSubview(googleSignInButton)
+        addSubview(hwibariImageView)
+        addSubview(label)
+
+        hwibariImageView.snp.makeConstraints { make in
+            make.width.equalTo(93)
+            make.height.equalTo(131)
+            make.leading.equalToSuperview().offset(42)
+            make.top.equalToSuperview().offset(246)
+        }
+
+        label.snp.makeConstraints { make in
+            make.width.equalTo(248)
+            make.height.equalTo(80)
+            make.top.equalTo(hwibariImageView.snp.bottom).offset(27)
+            make.leading.equalToSuperview().offset(36)
+            make.trailing.equalToSuperview().offset(-109)
+        }
         
-        googleSignInButton.addTarget(self, action: #selector(handleGoogleSignIn), for: .touchUpInside)
         googleSignInButton.snp.makeConstraints { make in
-            make.center.equalTo(self)
+            make.width.equalTo(333)
+            make.height.equalTo(56)
+            make.top.equalTo(label.snp.bottom).offset(54)
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
         }
     }
 
