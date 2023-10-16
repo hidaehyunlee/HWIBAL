111ea6c6d42ec76119fce253e7ece28dca3a691b
Seoon KIM <6872065@gmail.com>
2023-10-16 17:16:50 +0900

    [FEAT] : 👍 Report Bar Chart 구현 완료

--- HWIBAL/Views/ReportView/ReportSummaryCell.swift
+++ HWIBAL/Views/ReportView/ReportSummaryCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import Charts
+import DGCharts
 
 class ReportSummaryCell: UICollectionViewCell {
     static let identifier = "summaryCell"
@@ -44,11 +45,52 @@ class ReportSummaryCell: UICollectionViewCell {
         return label
     }()
     
+    let chartView: BarChartView = {
+        let chartView = BarChartView()
+        chartView.isUserInteractionEnabled = false
+        chartView.xAxis.drawGridLinesEnabled = false
+        chartView.xAxis.granularity = 1
+        chartView.xAxis.axisLineWidth = 0
+        chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: ["나", "평균"])
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.labelFont = FontGuide.size16Bold
+        chartView.xAxis.labelTextColor = ColorGuide.subButton
+        
+        chartView.leftAxis.enabled = false
+        chartView.leftAxis.drawGridLinesEnabled = false
+        chartView.leftAxis.axisMinimum = 0
+        
+        chartView.rightAxis.enabled = false
+        chartView.legend.enabled = false
+        chartView.animate(xAxisDuration: 2, yAxisDuration: 2)
+        chartView.snp.makeConstraints { make in
+            make.width.equalTo(295)
+        }
+        return chartView
+    }()
+    
+    let unit: UILabel = {
+        let label = UILabel()
+        label.text = "단위: 개"
+        label.font = FontGuide.size14
+        label.textColor = ColorGuide.textHint
+        label.textAlignment = .right
+        label.snp.makeConstraints { make in
+            make.width.equalTo(50)
+        }
+        return label
+    }()
+   
     public func configure() {
+        // MARK: - initializeUI
+        initializeUI()
+        
+        // MARK: - Title, SubTitle
         title.text = """
                      가입 이후 작성한
                      감쓰 \(totalEmotionTrashCount)개
                      """
+        
         switch totalEmotionTrashCount {
         case let difference where difference > averageEmotionTrashCount:
             subTitle.text = "평균보다 \(difference - averageEmotionTrashCount)개 더 썼어요"
@@ -57,7 +99,20 @@ class ReportSummaryCell: UICollectionViewCell {
         default:
             subTitle.text = "평균과 같아요"
         }
-        initializeUI()
+        
+        // MARK: - Chart
+        let totalEntry = BarChartDataEntry(x: 0, y: Double(totalEmotionTrashCount))
+        let averageEntry = BarChartDataEntry(x: 1, y: Double(averageEmotionTrashCount))
+        
+        let dataSet = BarChartDataSet(entries: [totalEntry, averageEntry], label: "감쓰 개수")
+        dataSet.colors = [ColorGuide.main, ColorGuide.textHint]
+        dataSet.valueColors = [ColorGuide.main, ColorGuide.textHint]
+
+        let data = BarChartData(dataSet: dataSet)
+        data.setValueFont(FontGuide.size32Bold)
+        chartView.data = data
+        chartView.barData?.barWidth = 0.3
+        
     }
     
     func initializeUI() {
@@ -78,6 +133,20 @@ class ReportSummaryCell: UICollectionViewCell {
         subTitle.snp.makeConstraints { make in
             make.top.equalTo(title.snp.bottom).offset(20)
             make.leading.equalToSuperview().offset(25)
+        }
+        
+        view.addSubview(chartView)
+        chartView.snp.makeConstraints { make in
+            make.top.equalTo(subTitle.snp.bottom).offset(60)
+            make.bottom.equalToSuperview().offset(-40)
+            make.centerX.equalToSuperview()
+        }
+        
+        view.addSubview(unit)
+        unit.snp.makeConstraints { make in
+            make.top.equalTo(chartView.snp.bottom).offset(10)
+            make.trailing.equalToSuperview().offset(-13)
+            make.bottom.equalToSuperview().offset(-13)
         }
     }
 }
