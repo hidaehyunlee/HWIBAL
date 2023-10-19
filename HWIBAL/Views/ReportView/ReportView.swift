9ba29cfcaaeb18d36cd504a74bad7ae1b2192ed4
Seoon KIM <6872065@gmail.com>
2023-10-19 22:49:07 +0900

    [FEAT] : 👍 ReportView 컬렉션뷰 스크롤 페이징 효과 적용

--- HWIBAL/Views/ReportView/ReportView.swift
+++ HWIBAL/Views/ReportView/ReportView.swift
@@ -10,7 +10,6 @@ import SnapKit
 
 final class ReportView: UIView, RootView {
     var currentPage = 1
-    var totalPage = 3
     
     let closeButton = CloseButton(color: .white)
     
@@ -30,11 +29,19 @@ final class ReportView: UIView, RootView {
         return button
     }()
     
+    var title: UILabel = {
+        let label = UILabel()
+        label.text = "\(SignInService.shared.signedInUser?.name ?? "사용자") 님의 리포트"
+        label.font = FontGuide.size14Bold
+        label.textColor = .white
+        return label
+    }()
+    
     lazy var numberOfPage: UILabel = {
         let label = UILabel()
         
-        let currentPageText = "\(self.currentPage) "
-        let totalPageText = "/ \(self.totalPage)"
+        let currentPageText = "\(currentPage) "
+        let totalPageText = "/ 3"
         
         let attributedString = NSMutableAttributedString(string: currentPageText, attributes: [
             .font: FontGuide.size16Bold,
@@ -55,11 +62,22 @@ final class ReportView: UIView, RootView {
     }()
     
     let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(538))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .none
+                
+            return section
+        }
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .black
         collectionView.showsVerticalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        collectionView.decelerationRate = .fast
         collectionView.register(ReportSummaryCell.self, forCellWithReuseIdentifier: ReportSummaryCell.identifier)
         collectionView.register(ReportDayOfTheWeekCell.self, forCellWithReuseIdentifier: ReportDayOfTheWeekCell.identifier)
         collectionView.register(ReportTimeZoneCell.self, forCellWithReuseIdentifier: ReportTimeZoneCell.identifier)
@@ -91,6 +109,13 @@ final class ReportView: UIView, RootView {
             make.trailing.equalToSuperview().offset(-25)
         }
         
+        addSubview(title)
+        title.snp.makeConstraints { make in
+            make.top.equalTo(closeButton.snp.bottom).offset(38)
+            make.leading.equalToSuperview().offset(35)
+            make.height.equalTo(20)
+        }
+        
         addSubview(numberOfPage)
         numberOfPage.snp.makeConstraints { make in
             make.top.equalTo(closeButton.snp.bottom).offset(38)
@@ -101,7 +126,9 @@ final class ReportView: UIView, RootView {
         addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(numberOfPage.snp.bottom).offset(10)
-            make.leading.trailing.bottom.equalToSuperview()
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.bottom.equalToSuperview()
         }
     }
 }
