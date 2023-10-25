5dacb05236a589987901533e866b274a3f387199
Seoon KIM <6872065@gmail.com>
2023-10-25 17:33:17 +0900

    [FIX] : 🐛 자동 휘발 설정 수정

--- HWIBAL/Services/NotificationService.swift
+++ HWIBAL/Services/NotificationService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UserNotifications
+import UIKit
 
 class NotificationService: NSObject, UNUserNotificationCenterDelegate {
     static let shared = NotificationService()
@@ -16,21 +17,14 @@ class NotificationService: NSObject, UNUserNotificationCenterDelegate {
         UNUserNotificationCenter.current().delegate = self
     }
     
-    func autoDeleteNotification(_ day: Int) {
+    func autoDeleteNotification() {
         let content = UNMutableNotificationContent()
         content.badge = 1
         content.title = "휘발이가 모든 감정쓰레기를 비웠어요!"
         content.body = "지금 눌러서 확인하기"
         content.sound = .default
-        
-        var dateComponents = DateComponents()
-        dateComponents.timeZone = TimeZone(identifier: "Asia/Seoul")
-        dateComponents.day = day
-        dateComponents.hour = 0
-        dateComponents.minute = 0
 
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
         let request = UNNotificationRequest(identifier: "deleteNotification", content: content, trigger: trigger)
 
         UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
@@ -43,14 +37,25 @@ class NotificationService: NSObject, UNUserNotificationCenterDelegate {
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
-        [.list, .banner, .sound, .badge]
+        print("푸시 알람 표시됨")
+        DispatchQueue.main.async {
+            print("삭제 로직 실행")
+            EmotionTrashService.shared.deleteTotalEmotionTrash(SignInService.shared.signedInUser!)
+            NotificationCenter.default.post(name: NSNotification.Name("EmotionTrashUpdate"), object: nil)
+        }
+        
+        return [.list, .banner, .sound, .badge]
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         if response.notification.request.identifier == "deleteNotification" {
-            print("푸시 알림을 눌러야 삭제됨")
-            EmotionTrashService.shared.deleteTotalEmotionTrash(SignInService.shared.signedInUser!)
-            NotificationCenter.default.post(name: NSNotification.Name("EmotionTrashUpdate"), object: nil)
+            
+            // 알림 확인 시 뱃지 카운트 없애기
+            UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+            UIApplication.shared.applicationIconBadgeNumber = 0
+            
+            // HomeVC 화면 전환 코드 추가
+            
         }
         completionHandler()
     }
