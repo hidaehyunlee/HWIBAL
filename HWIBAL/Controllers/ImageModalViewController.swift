e65cbed285b7363e6ea3de4024f84ea630e9f060
hidaehyunlee <hidaehyunlee@gmail.com>
2023-11-06 17:06:20 +0900

    [FEAT]: 👍  add ImageModalView, delete EmotionTrashBackView

---
+++ HWIBAL/Controllers/ImageModalViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModalViewController.swift
+//  HWIBAL
+//
+//  Created by daelee on 11/6/23.
+//
+
+import Foundation
---
+++ HWIBAL/Views/DetailView/ImageModalView.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModalView.swift
+//  HWIBAL
+//
+//  Created by daelee on 11/6/23.
+//
+
+import Foundation
